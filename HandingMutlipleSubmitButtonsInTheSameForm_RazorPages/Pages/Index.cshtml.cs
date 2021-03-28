8580ad6049b2477503e8245d38a1a62322cc2f68
Anas Al Kala <anas.kala@hotmail.com>
2021-03-28 13:38:25 +0200

    Add project files.

---
+++ HandingMutlipleSubmitButtonsInTheSameForm_RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HandingMutlipleSubmitButtonsInTheSameForm_RazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Msg { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostWork1()
+        {
+            Msg = "work 1";
+        }
+        public void OnPostWork2()
+        {
+            Msg = "work 2";
+        }
+
+        public void OnPostWork3()
+        {
+            Msg = "work 3";
+        }
+    }
+}
+
